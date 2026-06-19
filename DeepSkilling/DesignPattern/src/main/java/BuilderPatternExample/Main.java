d5cbfadfb0dfa337dd30ff596f9dcf7a08b1f0e3
Amol Dawane <amol.d@tesseringlobal.com>
2026-06-19 21:28:31 +0530

    builder pattern completed

---
+++ DeepSkilling/DesignPattern/src/main/java/BuilderPatternExample/Main.java
@@ -0,0 +1,17 @@
+package BuilderPatternExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer =  new Computer.ComputerBuilder().setCpuName("RYZEN 7") // computer obj with all paramerters with help of the builder method
+                                                            .setRamSize(16).setDiskSize(512)
+                                                            .createComputer();
+
+        System.out.println(computer);
+
+        Computer computer1 = new Computer.ComputerBuilder() // computer obj without ram size
+                .setCpuName("Intel I7")
+                .setDiskSize(512)
+                .createComputer();
+        System.out.println(computer1);
+    }
+}
